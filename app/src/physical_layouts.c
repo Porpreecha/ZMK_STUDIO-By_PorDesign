4af371a45e4f0318159da74d5f63fb841e1f5c9a
Porpreecha <preecha.pairoe@gmail.com>
2025-10-25 14:28:53 +0700

    Update physical_layouts.c

--- app/src/physical_layouts.c
+++ app/src/physical_layouts.c
@@ -1,11 +1,21 @@
+#include <device.h>
+#include <drivers/keyboard.h>
+#include <dt-bindings/zmk/keys.h>
+
 #ifdef CONFIG_ZMK_STUDIO
 #include <zmk/studio.h>
 #endif
 
-BUILD_ASSERT(
+// Define physical layout(s)
+const struct zmk_keymap_layout my_layouts[] = {
+    // ตัวอย่าง layout ของคุณ
+    { /* แถว 1 */ &kp A, &kp B, &kp C },
+    { /* แถว 2 */ &kp D, &kp E, &kp F },
+    // เพิ่มตามจำนวนแถว/คอลัมน์จริง
+};
+
+const size_t my_layout_count = sizeof(my_layouts) / sizeof(my_layouts[0]);
+
 #ifdef CONFIG_ZMK_STUDIO
-    1  // แสดงว่า keyboard นี้รองรับ Studio
-#else
-    1
+ZMK_STUDIO_KEYBOARD(PORDESIGN_5X7, my_layouts, my_layout_count);
 #endif
-);
